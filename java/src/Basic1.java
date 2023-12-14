264c47f55a8acce81d10019654284627ec68ab05
Eunsuk Kim <eunsukkim84@gmail.com>
2023-12-14 17:45:33 +0900

    Initial commit

---
+++ java/src/Basic1.java
@@ -0,0 +1,20 @@
+/**
+ * @프로젝트명        : Default (Template) Project
+ * @클래스명        : ${NAME}
+ * @패키지 정보  : 
+ * @설명        :
+ * @작성자 eunsukkim
+ * @작성일        : 14/12/23 11:22 AM
+ * @version v.0.1
+ */
+public class Basic1 {
+	public static void main(String[] args) {
+
+		System.out.println("haha");
+
+		System.out.println("Hello world!");
+
+
+
+	}
+}
